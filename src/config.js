b474fac558af672b40bb99a2ce36f9b29532d2ac
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-02 13:55:44 +0100

    Updated cognito IDs for connecting to backend

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ const dev = {
   },
   cognito: {
     REGION: "eu-west-2",
-    USER_POOL_ID: "eu-west-2_XNks9DojJ",
-    APP_CLIENT_ID: "2adh0he4jaicl9v6l3q9g516fk",
+    USER_POOL_ID: "eu-west-2_NYT6M8Gnk",
+    APP_CLIENT_ID: "27sal4iifa9mbug9as54gisjv9",
     IDENTITY_POOL_ID: "eu-west-2:6f32ac80-cd30-491b-99c9-b181f56eb8bb"
   }
 
