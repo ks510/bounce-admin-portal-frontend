9a0e095c1324d276fdd5f0839aae262635e1d1a6
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-03 16:55:04 +0100

    Created new container file for ForgotPassword components

---
+++ src/containers/ForgotPassword.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./ForgotPassword.css";
+
+export default class ForgotPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      confirmationCode: "",
+      isLoading: true
+    };
+  }
+
+  //TODO: render form for resetting password
+  // Email field, confirmation code will be sent by email to start the procecs
+  // change password by confirming with code and new password
+}
