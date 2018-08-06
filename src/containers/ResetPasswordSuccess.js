32192f3b9f44804ccd5ff47967c3a25f84088b2b
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-06 16:11:45 +0100

    Added new Reset Password Success page and route

---
+++ src/containers/ResetPasswordSuccess.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class ResetPasswordSuccess extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h3>Password Successfully Reset.</h3>
+    );
+  }
+}
