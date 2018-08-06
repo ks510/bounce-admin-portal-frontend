770c00f7057c010dde7a02b04d9c3219ac50345c
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-06 13:03:05 +0100

    Added email field to send password reset code and css styling for form

--- src/containers/ForgotPassword.js
+++ src/containers/ForgotPassword.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import "./ForgotPassword.css";
+import {
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import LoaderButton from "../components/LoaderButton";
 
 export default class ForgotPassword extends Component {
   constructor(props) {
@@ -8,12 +16,93 @@ export default class ForgotPassword extends Component {
     this.state = {
       email: "",
       confirmationCode: "",
-      isLoading: true
+      codeSent: false,
+      isLoading: false
     };
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  /**
+   * Request Cognito to send a verification code to user's email
+   **/
+  handleEmailSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ isLoading: true });
+
+    try {
+      await Auth.forgotPassword(this.state.email);
+      this.setState({ codeSent: true });
+    } catch (e) {
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
+  }
+
+  /**
+   * Ensures the email field cannot be empty.
+   **/
+  validateEmailForm() {
+    return this.state.email.length > 0;
+  }
+
   render() {
-    return (<p>Lets reset your Password</p>);
+    return (
+      <div className="ForgotPassword">
+        {this.state.codeSent
+          ? this.renderResetPasswordForm()
+          : this.renderEmailForm()}
+      </div>
+    );
+  }
+
+  renderEmailForm2() {
+    return (
+      <p>Please enter the email of your account to reset your password.</p>
+    );
+  }
+
+  renderResetPasswordForm() {
+    return (
+      <p>Please enter the confirmation code and your new password.</p>
+    );
+  }
+
+  renderEmailForm() {
+    return (
+      <div className="EmailForgotPasswordForm">
+        <form onSubmit={this.handleEmailSubmit}>
+          <h3>Reset your password.</h3>
+          <p>Please enter the email of your account to reset your password.
+          You&#39;ll receive an email within a few minutes to with a code for
+          resetting your account password. Please enter the code below.</p>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              placeholder="johnsmith@bouncebot.io"
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateEmailForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Send Code"
+            loadingText="Sending Code..."
+          />
+        </form>
+      </div>
+    );
   }
 
   //TODO: render form for resetting password
