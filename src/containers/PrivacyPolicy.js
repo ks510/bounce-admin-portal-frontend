efc759c58a8c3cfb4a6bd776943b080743fc0fbb
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-13 14:22:14 +0100

    Added markdown file from github to Terms of Service and Privacy Policy page

--- src/containers/PrivacyPolicy.js
+++ src/containers/PrivacyPolicy.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import marked from "marked";
+import ReactMarkdown from "react-markdown";
+import markdownFile from "../privacy-policy/Bounce - Privacy Policy.md";
+
 
 /**
  * Placeholder for display Bounce's privacy policy for users to view.
@@ -6,9 +10,25 @@ import React, { Component } from "react";
 export default class PrivacyPolicy extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      markdown: null
+    }
+  }
+
+  componentWillMount() {
+    fetch(markdownFile)
+      .then((r) => r.text())
+      .then(text  => {
+        this.setState({ markdown: text });
+      })
   }
 
   render() {
-    return <h3>Bounce&#39;s Privacy Policy...</h3>;
+    return (
+      <div className="privacy-policy">
+        <ReactMarkdown source={this.state.markdown} />
+      </div>
+    );
   }
 }
--- src/containers/TermsOfService.js
+++ src/containers/TermsOfService.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
+import marked from "marked";
+import ReactMarkdown from "react-markdown";
+import markdownFile from "../terms-of-service/Bounce - Terms of Service.md";
+
 
 /**
- * Placeholder for display Bounce's terms of service agreement for users to view.
+ * Placeholder for display Bounce's privacy policy for users to view.
  */
-export default class TermsOfService extends Component {
+export default class PrivacyPolicy extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      markdown: null
+    }
+  }
+
+
+  componentWillMount() {
+    // load the text from file and store it in component state
+    fetch(markdownFile)
+      .then((r) => r.text())
+      .then(text  => {
+        this.setState({ markdown: text });
+      })
   }
 
   render() {
-    return <h3>Bounce&#39;s Terms of Service...</h3>;
+    return (
+      <div className="privacy-policy">
+        <ReactMarkdown source={this.state.markdown} />
+      </div>
+    );
   }
 }
