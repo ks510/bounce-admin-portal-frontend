05d2981fe8a01ed252246208117219d7c6003db2
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-14 17:40:16 +0100

    Updated stripe test keys

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const dev = {
-  STRIPE_KEY: "pk_test_56VFAcEKqmW60sJdmfAb0bFs",
+  STRIPE_KEY: "pk_test_sMqwDrU78tzHpaMyEjPW4lk6",
   s3: {
     REGION: "eu-west-2",
     BUCKET: "admin-portal-dev-attachmentsbucket-fm4i68pe3llc"
@@ -18,7 +18,7 @@ const dev = {
 };
 
 const prod = {
-  STRIPE_KEY: "pk_test_56VFAcEKqmW60sJdmfAb0bFs",
+  STRIPE_KEY: "pk_test_sMqwDrU78tzHpaMyEjPW4lk6",
   s3: {
     REGION: "eu-west-2",
     BUCKET: "admin-portal-prod-attachmentsbucket-14rvb84isit9f"
