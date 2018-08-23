a24cf9feb3a1d4fbbccda7a5e1b4470c8b9cba34
Karen Suen <karen@Nats-MacBook-Air.local>
2018-08-23 20:24:30 +0100

    Documented ResetPasswordSuccess.js

--- src/containers/ResetPasswordSuccess.js
+++ src/containers/ResetPasswordSuccess.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import LinkButton from "../components/LinkButton";
 import "./ResetPasswordSuccess.css";
 
+/**
+ * Renders a message notifying the user of successful password reset. A button
+ * is provided for navigating to portal landing.
+ **/
 export default class ResetPasswordSuccess extends Component {
 
   render() {
